3a306a3a171f97881ae0d653a3231eb35671744d
Brandon Pitts <Brandomoki.bp@gmail.com>
2022-11-01 23:34:19 +0000

    completed lab-31

--- src/Components/ToDo/index.js
+++ src/Components/ToDo/index.js
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import useForm from '../../hooks/form.js';
-
+import useForm from '../../hooks/form'
+import AppHeader from '../Header/header';
+import AppFooter from '../Footer/footer';
 import { v4 as uuid } from 'uuid';
+import List from '../List/list'
+import { Button, Card, createStyles, Grid, Slider, Text, TextInput } from '@mantine/core';
+
+
+const useStyles = createStyles((theme) => ({
+  formHeading: {
+    fontSize: theme.fontSizes.lg,
+    fontWeight: 'bold',
+  },
+}));
 
 const ToDo = () => {
+
+  const { classes } = useStyles();
 
   const [defaultValues] = useState({
     difficulty: 4,
@@ -20,15 +33,15 @@ const ToDo = () => {
   }
 
   function deleteItem(id) {
-    const items = list.filter( item => item.id !== id );
+    const items = list.filter(item => item.id !== id);
     setList(items);
   }
 
   function toggleComplete(id) {
 
-    const items = list.map( item => {
-      if ( item.id === id ) {
-        item.complete = ! item.complete;
+    const items = list.map(item => {
+      if (item.id === id) {
+        item.complete = !item.complete;
       }
       return item;
     });
@@ -41,51 +54,56 @@ const ToDo = () => {
     let incompleteCount = list.filter(item => !item.complete).length;
     setIncomplete(incompleteCount);
     document.title = `To Do List: ${incomplete}`;
-    // linter will want 'incomplete' added to dependency array unnecessarily. 
-    // disable code used to avoid linter warning 
-    // eslint-disable-next-line react-hooks/exhaustive-deps 
-  }, [list]);  
+  }, [list]);
 
   return (
     <>
-      <header data-testid="todo-header">
-        <h1 data-testid="todo-h1">To Do List: {incomplete} items pending</h1>
-      </header>
+      <AppHeader incomplete={incomplete} />
+      <Grid style={{ width: '80%', margin: 'auto' }}>
+        <Grid.Col xs={12} sm={4}>
+          <Card>
+            <Text className={classes.formHeading}>Add To Do Item</Text>
 
-      <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
 
-        <h2>Add To Do Item</h2>
+              <TextInput
+                placeholder="Item Details"
+                name="text"
+                onChange={handleChange}
+                label="To Do Item"
+              />
 
-        <label>
-          <span>To Do Item</span>
-          <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
-        </label>
+              <TextInput
+                placeholder="Assignee Name"
+                name="assignee"
+                onChange={handleChange}
+                label="Assigned To"
+              />
 
-        <label>
-          <span>Assigned To</span>
-          <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-        </label>
+              <Text>Difficulty</Text>
+              <Slider
+                onChange={handleChange}
+                defaultValue={defaultValues.difficulty}
+                min={0}
+                max={5}
+                step={1}
+                name="difficulty"
+                type="range"
+                mb="lg"
+              />
 
-        <label>
-          <span>Difficulty</span>
-          <input onChange={handleChange} defaultValue={defaultValues.difficulty} type="range" min={1} max={5} name="difficulty" />
-        </label>
+              <Button type="submit">Add Item</Button>
 
-        <label>
-          <button type="submit">Add Item</button>
-        </label>
-      </form>
+            </form>
 
-      {list.map(item => (
-        <div key={item.id}>
-          <p>{item.text}</p>
-          <p><small>Assigned to: {item.assignee}</small></p>
-          <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-          <hr />
-        </div>
-      ))}
+          </Card>
+        </Grid.Col>
 
+          <Grid.Col xs={12} sm={8}>
+            <List list={list} toggleComplete={toggleComplete}></List>
+          </Grid.Col>
+      </Grid>
+      <AppFooter />
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import SettingsProvider from './Context/Settings/settingsContext';
+import { MantineProvider } from '@mantine/core';
 
-import App from './app';
+import App from './app.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles with withNormalizeCSS>
+      <SettingsProvider>
+        <App />
+      </SettingsProvider>
+    </MantineProvider>
   </React.StrictMode>
 );
